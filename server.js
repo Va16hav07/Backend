a4dfe611d179e4c0638a25122d5982748f7f22af
Vaibhav Kumawat <vaibhavkumawat7605@gmail.com>
2024-10-10 10:48:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/user', (req, res) => {
     const user = {
-        firstName: "Vaibhav",
-        lastName: "Kumawat",
-        email: "vaibhavkumawat7605@gmail.com"
+        firstName: "Firstname",
+        lastName: "Lastname",
+        email: "Write the email"
     };
     res.json(user);
 });
